86737048a6aa2f1b4de436eef97dba9689660736
Stan Chen <stanchensz@gmail.com>
2022-02-16 17:46:42 -0700

    added examples of how to load data into dataframe for analytics

--- work/S3ReaderApp/src/main/scala/S3Reader/s3Executor.scala
+++ work/S3ReaderApp/src/main/scala/S3Reader/s3Executor.scala
@@ -1,6 +1,9 @@
 package S3Reader
 
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.DataFrame
+
+import scala.util.parsing.json
 //  ref https://sparkbyexamples.com/spark/spark-read-text-file-from-s3/
 object s3Executor {
   def execute(args: Array[String]): RDD[String] = {
@@ -19,13 +22,16 @@ object s3Executor {
     spark.sparkContext
       .hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
     val s3RDD = spark.sparkContext.textFile(s3Path)
-
+    val s3DF = spark.read.option("multiline", "true").json(s3Path)
     // Need to add configuration (JSON settings need it from Stan) for GCS file system.
     // Need to include Google spark + google file transfer in Pom file for maven project
     // Then point to the this configuration the JSON file (obtained by GCS bucket settings).
-    s3RDD.SparkContext.hadoopConfiguration.set("google.cloud.auth.service.account.json.keyfile","<POINT TO KeyFile Path>")
-    // Then write to gs://folder
-    s3RDD.saveAsTextFile("link to gcs")
+//    s3RDD.SparkContext.hadoopConfiguration.set("google.cloud.auth.service.account.json.keyfile","<POINT TO KeyFile Path>")
+//    // Then write to gs://folder
+//    s3RDD.saveAsTextFile("link to gcs")
+//    s3DF.printSchema()
+    s3DF.show()
+    s3RDD
   }
 
 
@@ -33,7 +39,7 @@ object s3Executor {
     // define commons, best to get credentials from environment variables
     val accessKeyID = System.getenv("ACCESS_KEY_ID")
     val secretAccessKey = System.getenv("SECRET_ACCESS_KEY")
-    val s3Path = "s3a://skip-capstone-2022/grocery_order_transaction_data/*.json"
+    val s3Path = "s3a://skip-capstone-2022/grocery_order_transaction_data/24296*.json"
     val s3ResultRDD = execute(Array(accessKeyID,
       secretAccessKey,
       s3Path
