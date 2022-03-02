933f79df52f1e998097f3e0bf966c40a9a1d4f20
Erika Wang <801erikawang@gmail.com>
2022-03-01 22:31:26 -0700

    imported libraries

--- work/S3ReaderApp/src/main/scala/S3Reader/s3Executor.scala
+++ work/S3ReaderApp/src/main/scala/S3Reader/s3Executor.scala
@@ -9,7 +9,8 @@ import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.functions.monotonically_increasing_id
 
 //  ref https://sparkbyexamples.com/spark/spark-read-text-file-from-s3/
-case class Test(key:String, value:String)
+case class Test(key: String, value: String)
+
 object s3Executor {
   def execute(args: Array[String]): DataFrame = {
     import org.apache.spark.sql.SparkSession
@@ -28,15 +29,15 @@ object s3Executor {
       .hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
 
 
-//    spark.conf.set("google.cloud.auth.service.account.enable", "true")
-//    spark.conf.set("google.cloud.auth.service.account.email", System.getenv("ACCOUNT_EMAIL"))
-//    spark.conf.set("google.cloud.auth.service.account.keyfile", System.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+    //    spark.conf.set("google.cloud.auth.service.account.enable", "true")
+    //    spark.conf.set("google.cloud.auth.service.account.email", System.getenv("ACCOUNT_EMAIL"))
+    //    spark.conf.set("google.cloud.auth.service.account.keyfile", System.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
     import spark.implicits._
     val s3RDD = spark.sparkContext.textFile(s3Path)
 
     val rdd_new = s3RDD
       .filter(f => !f.equals("{") && !f.equals("}")).map(x => x.split(":"))
-      .map(x => Test(x(0).strip(),x(1).replace(",","")))
+      .map(x => Test(x(0).strip(), x(1).replace(",", "")))
 
     val rdd1 = rdd_new.filter(x => x.key.equals("\"order_id\"")).map(x => x.value)
     val rdd2 = rdd_new.filter(x => x.key.equals("\"custo_id\"")).map(x => x.value)
@@ -45,20 +46,20 @@ object s3Executor {
     val allRDDs = Seq(rdd1, rdd2, rdd3, rdd4)
     val res = applyFuncToZip(allRDDs, (s: Seq[String]) => s.toString())
 
-//    res.foreach(s => println(s)) // TODO: construct an object with right format and save to gcs instead of println
+    //    res.foreach(s => println(s)) // TODO: construct an object with right format and save to gcs instead of println
 
-//    //convert rdd to json and write to gcs
-//    res.toDF("order_id", "custo_id", "order.item_grp_id", "order.qty").write.format("parquet").save(System.getenv("GCS_BUCKET"))
+    //    //convert rdd to json and write to gcs
+    //    res.toDF("order_id", "custo_id", "order.item_grp_id", "order.qty").write.format("parquet").save(System.getenv("GCS_BUCKET"))
 
 
-//    //read multiline json files as a df and write to gcs
+    //    //read multiline json files as a df and write to gcs
     val s3DF = spark.read.option("multiline", "true").json(s3Path)
 
     saveToGCS(s3DF)
 
     s3DF
-//    //write to gcs
-//    s3DF.write.format("parquet").save(System.getenv("GCS_BUCKET"))
+    //    //write to gcs
+    //    s3DF.write.format("parquet").save(System.getenv("GCS_BUCKET"))
   }
 
   def saveToGCS(s3DF: DataFrame): Unit = {
@@ -66,14 +67,21 @@ object s3Executor {
       .master("local")
       .appName("SaveToGCS")
       .getOrCreate()
+
+//    https://stackoverflow.com/questions/32591745/save-spark-dataframe-as-parquet-file-in-google-cloud-storage
+    spark2.conf.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+//    spark2.conf.set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
     spark2.conf.set("google.cloud.auth.service.account.enable", "true")
     spark2.conf.set("google.cloud.auth.service.account.email", System.getenv("ACCOUNT_EMAIL"))
     spark2.conf.set("google.cloud.auth.service.account.keyfile", System.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 
-//    spark2.conf.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
-//    spark2.conf.set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
     //write to gcs
-    s3DF.write.format("parquet").save(System.getenv("GCS_BUCKET"))
+    s3DF.write.format("parquet")
+      .partitionBy("order_id")
+      .save(System.getenv("GCS_BUCKET"))
+
+    //    //to csv
+    //    s3DF.coalesce(1).write.mode("overwrite").csv(System.getenv("GCS_BUCKET"))
   }
 
   // zip the RDDs into an RDD of Seq[String]
@@ -88,7 +96,7 @@ object s3Executor {
   }
 
   // zip and apply arbitrary function from Seq[String] to String
-  def applyFuncToZip(s: Seq[RDD[String]], f:Seq[String] => String): RDD[String] = {
+  def applyFuncToZip(s: Seq[RDD[String]], f: Seq[String] => String): RDD[String] = {
     val z = makeZip(s)
     z.map(f)
   }
